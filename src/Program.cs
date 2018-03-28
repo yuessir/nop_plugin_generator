a9995140703c414cd8be0a871e2b28c0f7194fa1
BSS\showell <showell@bayshoresolutions.com>
2018-03-28 11:29:05 -0600

    initial

---
+++ src/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BASPluginTemplator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
